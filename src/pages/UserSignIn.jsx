42f70654b0e906c89018805364abeca210501220
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-03-07 20:11:28 +0000

    UserSignIn page styles added

--- src/pages/UserSignIn.jsx
+++ src/pages/UserSignIn.jsx
@@ -1,6 +1,7 @@
 import logo from "../assets/images/umat-logo.png";
 import { ReactComponent as CorrectIcon } from "../assets/svgs/CorrectSign.svg";
 import { ReactComponent as UserIcon } from "../assets/svgs/UserIcon.svg";
+import { ReactComponent as PasswordIcon } from "../assets/svgs/PasswordIcon.svg";
 
 const UserSignIn = () => {
   return (
@@ -10,34 +11,49 @@ const UserSignIn = () => {
           <h1 className="heading">SIGN IN</h1>
           <CorrectIcon width="10rem" className="checkMark" />
         </div>
-        <form>
-          <label htmlFor="username">
-            <UserIcon />
-          </label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Username"
-          />
-          <label htmlFor="username">
-            <UserIcon />
-          </label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Password"
-          />
+        <form className="form">
+          <div className="wrapper">
+            <label htmlFor="username">
+              <UserIcon className="icon" />
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+              className="inputField"
+            />
+          </div>
+          <div className="wrapper">
+            <label htmlFor="password">
+              <PasswordIcon className="icon" />
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              className="inputField"
+            />
+          </div>
 
-          <button type="submit">LOGIN</button>
+          <button className="auth" type="submit">
+            LOGIN
+          </button>
         </form>
-        <a href="#" target="_blank" rel="noreferrer">
-          forgot password
-        </a>
-        <a href="#" target="_blank" rel="noreferrer">
-          register
-        </a>
+        <div className="auth-other">
+          <a
+            href="#"
+            target="_blank"
+            rel="noreferrer"
+            className="forgotPassword"
+          >
+            forgot password
+          </a>
+          <a href="#" target="_blank" rel="noreferrer" className="register">
+            register
+          </a>
+        </div>
       </div>
       <img src={logo} alt="" className="logo" />
     </div>
