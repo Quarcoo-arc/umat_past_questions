275b1fb986f010a0db2ef2d8365b6a5a77563fa7
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-05-10 13:22:42 +0000

    Password Reset Page Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserSignIn from "./pages/UserSignIn";
 import UserRegistration from "./pages/UserRegistration";
+import PasswordRecovery from "./pages/PasswordRecovery";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<UserSignIn />} />
         <Route path="/register" element={<UserRegistration />} />
+        <Route path="/forgot-password" element={<PasswordRecovery />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/PasswordRecovery.jsx
@@ -0,0 +1,44 @@
+import logo from "../assets/images/umat-logo.png";
+import { ReactComponent as CorrectIcon } from "../assets/svgs/CorrectSign.svg";
+import { ReactComponent as EmailIcon } from "../assets/svgs/EmailIcon.svg";
+
+const PasswordRecovery = () => {
+  return (
+    <div className="background">
+      <div className="card">
+        <div className="container">
+          <h1 className="heading">PASSWORD RECOVERY</h1>
+          <CorrectIcon width="10rem" className="checkMark" />
+        </div>
+        <form className="form new">
+          <div className="wrapper row-center">
+            <label htmlFor="email">
+              <EmailIcon className="icon" />
+            </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Enter your email address"
+              className="inputField new xxlg"
+            />
+          </div>
+          <button className="auth new" type="submit">
+            SEND
+          </button>
+        </form>
+        <div className="auth-other new end">
+          <p className="forgotPassword">
+            Return to &nbsp;
+            <a href="./" className="register">
+              login
+            </a>
+          </p>
+        </div>
+      </div>
+      <img src={logo} alt="" className="logo" />
+    </div>
+  );
+};
+
+export default PasswordRecovery;
