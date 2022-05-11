96fd1931f4c187d7a0dc6f11e74360e4594528a0
Michael Quarcoo <michaelquarcoo04@gmail.com>
2022-05-11 22:55:11 +0000

    Landing Page added

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,18 @@
 import Header from "../components/Header";
+import books from "../assets/images/Books.png";
 
 const LandingPage = () => {
   return (
     <div>
       <div className="background-red">
         <Header isLoggedIn={false} isAdmin={false} />
-        <div className="content"></div>
+        <div className="content">
+          <img src={books} alt="" className="books" />
+          <div className="col new">
+            <p>Get access to Past Questions in all courses!</p>
+            <button className="auth">GET STARTED</button>
+          </div>
+        </div>
       </div>
     </div>
   );
