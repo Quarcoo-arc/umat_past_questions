5875b697bd7929e6096739819c0987ffce0d439e
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-06-14 09:12:50 +0000

    Add methods to add new faculties, programs, courses and past questions and retrieve them

--- src/context/QuestionsContext.js
+++ src/context/QuestionsContext.js
@@ -1,19 +1,134 @@
 import {
+  addDoc,
   arrayRemove,
   arrayUnion,
+  collection,
   doc,
   getDoc,
+  getDocs,
   setDoc,
+  where,
 } from "firebase/firestore";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { createContext, useState } from "react";
 import { db } from "../firebase.config";
+import { toast } from "react-toastify";
 
 const QuestionsContext = createContext();
 
 export const QuestionsContextProvider = ({ children }) => {
   const [questions, setQuestions] = useState([]);
   const [downloadUrls, setDownloadUrls] = useState([]);
+
+  // *******
+
+  const addFaculty = async (name) => {
+    const facultyRef = await addDoc(collection(db, "faculty"), {
+      name,
+    });
+    return facultyRef;
+  };
+
+  const addProgram = async (name, shortName, facultyId, numOfYears) => {
+    const programRef = await addDoc(collection(db, "program"), {
+      name,
+      short_name: shortName,
+      faculty_id: facultyId,
+      num_of_years: numOfYears,
+    });
+    console.log("Program created with ID: ", programRef.id);
+    return programRef;
+  };
+
+  const addCourse = async (name, programs, years, semesters) => {
+    const courseRef = await addDoc(collection(db, "course"), {
+      name,
+      programs,
+      years,
+      semesters,
+    });
+    return courseRef;
+  };
+
+  const addPastQuestion = async (fileURL, courseId) => {
+    const pastQuestionRef = await addDoc(collection(db, "past_question"), {
+      url: fileURL,
+      course_id: courseId,
+    });
+    return pastQuestionRef;
+  };
+
+  const uploadFile = async (file) => {
+    const storage = getStorage();
+    const storageRef = ref(storage, `past_questions/${file.name}`);
+    const fileUrl = await uploadBytes(storageRef, file)
+      .then((snapshot) => {
+        console.log("Uploaded file: " + file.name + " successfully!");
+        getDownloadURL(snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Failed to upload file: ", file.name);
+      });
+    return fileUrl;
+  };
+
+  const loadPrograms = async () => {
+    const querySnap = await getDocs(collection(db, "program"));
+    return querySnap;
+  };
+
+  const loadFaculties = async () => {
+    const querySnap = await getDocs(collection(db, "faculty"));
+    return querySnap;
+  };
+
+  const loadProgramCourses = async (programId) => {
+    const querySnap = await getDocs(
+      collection(db, "course"),
+      where("programs", "array-contains", programId)
+    );
+
+    const groupedCourses = {};
+    querySnap.forEach((item) => {
+      const data = item.data();
+      data.programs.forEach((id, idx) => {
+        if (id === programId) {
+          const year = data.years[idx];
+          const semester = data.semesters[idx];
+
+          if (!groupedCourses[year]) {
+            groupedCourses[year] = {};
+          }
+
+          if (!groupedCourses[year][semester]) {
+            groupedCourses[year][semester] = [];
+          }
+
+          groupedCourses[year][semester].push({
+            id: item.id,
+            name: data.name,
+            year: data.years[idx],
+            semester: data.semesters[idx],
+          });
+        }
+      });
+    });
+
+    return groupedCourses;
+  };
+
+  const loadPastQuestions = async (courseId) => {
+    const querySnap = await getDocs(
+      collection(db, "past_question"),
+      where("course_id", "==", courseId)
+    );
+    return querySnap;
+  };
+
+  // *******************************************///
 
   const loadQuestions = async (department, level, semester) => {
     try {
