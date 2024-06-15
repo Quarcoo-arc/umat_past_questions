5f755cb7f9f867561d215d36d4985576b6b105d7
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-06-15 05:05:58 +0000

    Add button component

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,4 +1,5 @@
 import ProgramButton from "./ProgramButton/ProgramButton";
 import RadioButton from "./RadioButton/RadioButton";
+import Button from "./Button/Button";
 
-export { ProgramButton, RadioButton };
+export { ProgramButton, RadioButton, Button };
